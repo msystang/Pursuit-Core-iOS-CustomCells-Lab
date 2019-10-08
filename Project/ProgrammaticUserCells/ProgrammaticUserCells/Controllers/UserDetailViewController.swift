2f0f580ffc5caca126e9f043954b39ff9465647f
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-08 17:56:08 -0400

    adds constraints to labels in userdetailVC

--- Project/ProgrammaticUserCells/ProgrammaticUserCells/Controllers/UserDetailViewController.swift
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/Controllers/UserDetailViewController.swift
@@ -13,27 +13,39 @@ class UserDetailViewController: UIViewController {
     var user: User!
     
     lazy var nameLabel: UILabel! = {
-           let label = UILabel()
-        label.text = user.name.first
-           return label
+        let label = UILabel(frame: CGRect(x: 100, y: 300, width: 400, height: 400))
+        label.text = "\(user.name.title) \(user.name.first) \(user.name.last)"
+        return label
+    }()
+    
+    lazy var emailLabel: UILabel! = {
+        let label = UILabel(frame: CGRect(x: 100, y: 350, width: 400, height: 400))
+        label.text = "\(user.email)"
+        return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(nameLabel)
-        configureConstraints()
-        
-        
         self.view.backgroundColor = .yellow
+        
+        addSubviews()
+        configureConstraints()
     }
     
+    private func addSubviews() {
+        self.view.addSubview(nameLabel)
+        self.view.addSubview(emailLabel)
+    }
 
     private func configureConstraints() {
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             nameLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-            nameLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)])
+            nameLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            emailLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 20 ),
+            emailLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 20)
+        ])
     }
 
 
